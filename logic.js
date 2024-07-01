3e3710da0317864a1188a7545b1ae01652511b6f
codewithsahu <sahanamisti@gmail.com>
2024-07-02 00:09:09 +0530

    second commit

--- logic.js
+++ logic.js
@@ -1,24 +1,27 @@
-      const targetDate = new Date('2025-01-01T00:00:00')
+const targetDate = new Date("2025-01-01");
 
-        function updateCountdown(){
-            const currentDate = new Date();
+function updateCountdown() {
+  const currentDate = new Date();
 
-            const timeDifference = targetDate - currentDate
+  const timeDifference = targetDate - currentDate;
 
-            console.log(timeDifference)
+  console.log(timeDifference);
 
-            const days = Math.floor(timeDifference/(1000 * 60 * 60 * 24));
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
-            const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-            
+  const hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
 
-            const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60))
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
 
-            const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000)
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-            document.getElementById('countdown').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`
+  document.getElementById(
+    "countdown"
+  ).innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
-            setTimeout(updateCountdown,1000)
-        }
+  setTimeout(updateCountdown, 1000);
+}
 
-        updateCountdown()
+updateCountdown();
